f608af08d8e8569e6e2dc238ecca9c66b0e9e3db
hansk0812 <hans94812@gmail.com>
2024-10-09 22:15:16 +0530

    added-checkpointing

--- analysis/fit_real_tsdata.py
+++ analysis/fit_real_tsdata.py
@@ -1,3 +1,5 @@
+import os
+
 from matplotlib import pyplot as plt
 
 from datasetsforecast.utils import Info
@@ -5,20 +7,48 @@ from datasetsforecast.long_horizon import LongHorizon
 from datasetsforecast.long_horizon import ETTh1, ETTh2, ETTm1, ETTm2, \
                                             ECL, Exchange, TrafficL, ILI, Weather
 
+from neuralforecast.models import NHITS
+from neuralforecast import NeuralForecast
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("--model_path", help="Path to model to load from", default=None)
+args = ap.parse_args()
+
+h = 32
+
+OUTLIER_THRESHOLDS = [
+                        [0,0], 
+                        [0,0],
+                        [0,0],
+                        [-6,-4.25],
+                        [20,15.3],
+                        [0,0],
+                        [0,0],
+                        [0,0],
+                        [-150,-30]
+                     ]
+
+TIME_INTERVALS = [
+                    "H",
+                    "H",
+                    "15m",
+                    "15m",
+                    "1h",
+                    "1d",
+                    "1h",
+                    "1w",
+                    "10m"
+                 ]
+
 def remove_outliers(data, idx):
 
-    OUTLIER_THRESHOLDS = [
-                            [0,0], 
-                            [0,0],
-                            [0,0],
-                            [-6,-4.25],
-                            [20,15.3],
-                            [0,0],
-                            [35,25],
-                            [0,0],
-                            [-150,-30]
-                         ]
-    
     if OUTLIER_THRESHOLDS[idx][0] == OUTLIER_THRESHOLDS[idx][1] and OUTLIER_THRESHOLDS[idx][0] == 0:
         return
     
@@ -45,6 +75,57 @@ for idx, (group, meta) in enumerate(LongHorizonInfo):
     #data["y"].plot()
     #plt.show()
     remove_outliers(data, idx)
-    data["y"].plot()
-    plt.show()
     
+    #data["y"].plot()
+    #plt.show()
+    #print (data["ds"])
+    
+    data["ds"] = pd.to_datetime(data["ds"])
+    
+    L = len(data)
+#    train_split = int(L * 0.8)
+#    train_data = data.loc[:train_split]
+#    test_data = data.loc[train_split:]
+    
+    msk = np.random.rand(len(data)) < 0.8
+    train_data, test_data = data[msk], data[~msk]
+    
+    checkpoint_callback = ModelCheckpoint(dirpath="checkpoints", save_top_k=2, monitor="valid_loss", filename="nhits-{epoch:02d}-{val_loss:.2f}")
+    models = [
+            NHITS(input_size=h, h=h, max_steps=200, start_padding_enabled=True, default_root_dir = "checkpoints", enable_checkpointing=True, callbacks = [checkpoint_callback])
+             ]
+    model_name = "NHITS"
+
+    if not args.model_path is None:
+        model = NHITS.load_from_checkpoint(args.model_path)
+        models[0] = model
+    
+    nf = NeuralForecast(models = models, freq = TIME_INTERVALS[idx])
+    
+    nf.fit(df=train_data, val_size=int(L * 0.1))
+
+    mae = []
+    for horizon in range(0, len(test_data), 2*h):
+        
+        try:
+            test_horizon_insample = pd.DataFrame()
+            test_horizon_insample["y"] = np.array(test_data["y"][horizon:horizon+2*h])
+            
+            test_horizon_insample["ds"] = np.array(test_data["ds"].iloc[horizon:horizon+2*h])
+            test_horizon_insample["unique_id"] = np.ones(2*h)
+
+            y_hat_forecast = nf.predict(df=test_horizon_insample)
+        
+        except ValueError:
+            break
+
+        #pred = np.concatenate([np.zeros(h//2), y_hat_forecast["NHITS"]]) 
+        #plt.plot(test_horizon_insample["y"])
+        #plt.plot(np.arange(h, h*2, 1), y_hat_forecast["NHITS"])
+        
+        mae.append(np.abs(np.array(test_horizon_insample["y"][h:]) - np.array(y_hat_forecast["NHITS"])).mean())
+        #plt.show()
+        #plt.cla(); plt.clf();
+
+    print ("START=%s STEP=%s Test set MAE = %.7f" % (os.environ["START"], os.environ["STEP"], np.array(mae).mean()))
+    exit()
